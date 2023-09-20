b66d7442c34cfbbb06c5d00d28adc91bde170945
ne5oo <106486790+neo5oo@users.noreply.github.com>
2023-09-20 18:52:27 +0300

    Реализация ошибки Попытка реализации списка контрагентов

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,19 @@
 import {initContractors} from './modules/contractors.js';
 import {initUserProfile} from './modules/user-profile.js';
 import {getData} from './modules/api.js';
+import {showErrorMessage} from './modules/error-message.js';
+import {addContractors} from './modules/add-contractors.js';
 
 Promise.all([getData('user'), getData('contractors')])
   .then(([userData, contractorsData]) => {
     initUserProfile(userData);
     initContractors(contractorsData);
   })
-  .catch(console.error);
+  .catch((err) => {
+    if (location.hostname === 'localhost') {
+      console.error(err.message);
+    }
+    showErrorMessage();
+  });
+
+addContractors();
